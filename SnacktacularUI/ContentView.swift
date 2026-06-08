a01689aad5a4111c707dbbf144783a8abdb35498
JimWallyj <jwalejko@me.com>
2026-06-08 12:33:10 -0700

    Initial Commit

---
+++ SnacktacularUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnacktacularUI
+//
+//  Created by JIM WALEJKO on 6/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
