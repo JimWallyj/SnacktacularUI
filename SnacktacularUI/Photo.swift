2afe8c66a8d144dc3bf5f0ffa25f7fa9bbcf1680
JimWallyj <jwalejko@me.com>
2026-07-22 21:10:49 -0700

    Save 6

--- SnacktacularUI/Photo.swift
+++ SnacktacularUI/Photo.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Firebase
 import FirebaseFirestore
-import FirebaseAuth
+
 
 class Photo: Identifiable, Codable {
     @DocumentID var id: String?
@@ -20,27 +20,6 @@ class Photo: Identifiable, Codable {
     var dictionary: [String: Any]{
         return ["imageURLString": imageURLString, "description": description, "reviewer": reviewer, "postedOn": Timestamp(date: Date())]
     }
-    
-//    init(id: String? = nil, imageURLString: String = "", description: String = "", reviewer: String = (Auth.auth().currentUser?.email ?? ""), postedOn: Date = Date()) {
-//        self.id = id
-//        self.imageURLString = imageURLString
-//        self.description = description
-//        self.reviewer = reviewer
-//        self.postedOn = postedOn
-//    }
-//    
-//}
-//
-//extension Photo{
-//    static var preview: Photo{
-//        let newPhoto = Photo(
-//            id: "1",
-//            imageURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/960px-Pizza-3007395.jpg",
-//            description: "Yummy Pizza",
-//            reviewer: "little@caesars.com",
-//            postedOn: Date()
-//        )
-//        return newPhoto
-//    }
+ 
 }
 
