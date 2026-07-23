2f15ab93af49bb732bea7171ae569ffa28c5ca8c
JimWallyj <jwalejko@me.com>
2026-07-22 20:55:33 -0700

    Save 3

--- SnacktacularUI/Models/Place.swift
+++ SnacktacularUI/Models/Place.swift
@@ -16,8 +16,7 @@ struct Place: Identifiable {
     init(mapItem: MKMapItem) {
         self.mapItem = mapItem
     }
-    
-}
+
 
 var name: String {
     self.mapItem.name ?? ""
@@ -30,3 +29,34 @@ var address: String {
 }
 
 cityAndState = placemark.locality ?? ""  //city
+if let state = placemark.administrativeArea {
+    //  show either state or city, state
+    cityAndState = cityAndState.isEmpty ? state : "\cityAndState), \(state)"
+}
+
+address = placement.subThoroughfare ?? "" //  address #
+if let street = placemark.thoroughfare {
+    //  Just show the street unless there is a street # then add space + street
+    address = address.isEmpty ? street : "\(address) \(street)"
+}
+
+if address.trimmingCharacters(in: .whitespaces).isEmpty && !addressCityAndState.isEmpty{
+    //  No address? Then just cityAndState with no space
+    address = addressCityAndState
+} else{
+    //  No cityAndState? Then just address, otherwise address, cityAndState
+    address = cityAndState.isEmpty ? address : "\(address), \(cityAndState)"
+}
+
+return address
+}
+
+var latitude: CLLocationDegrees{
+    self.mapItem.placemark.coordinate.latitude
+}
+
+var longitude: CLLocationDegrees{
+    self.mapItem.placemark.coordinate.longitude
+}
+}
+
