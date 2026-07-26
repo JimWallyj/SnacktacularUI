1de6175ed4637b43b97ec4ae19c8fdbd9c3c1781
JimWallyj <jwalejko@me.com>
2026-07-25 20:20:58 -0700

    Save 12

--- SnacktacularUI/ViewModels/LocationManager.swift
+++ SnacktacularUI/ViewModels/LocationManager.swift
@@ -16,65 +16,30 @@ class LocationManager: NSObject, ObservableObject{
     @Published var location: CLLocation?
 @Published var region = MKCoordinateRegion()
     
-
-    
+private let locationManager = CLLocationManager()
     
     override init(){
         super.init()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingLocation()  //  Remember to update Info.plist!
         locationManager.delegate = self
     }
     }
     
-    //  Get a region around current location with specified radius in meters
-    func getRegionAroundCurrentLocation(radiusInMeters: CLLocationDistance = 10000) -> MKCoordinateRegion? {
-        guard let location else { return nil }
-        
-        return MKCoordinateRegion(
-            center: location.coordinate,
-            latitudinalMeters: radiusInMeters,
-            longitudinalMeters: radiusInMeters
-        )
-    }
+    
 }
 
 //  Delegate methods that Apple has created & will call, but that we filled out
-extension LocationManager{
+extension LocationManager: CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let newLocation = locations.last else { return }  //  Use the last location as the location
-        location = newLocation
-        //  Call the callback function to indicate we've updated a location
-        locationUpdated?(newLocation)
-        
-        //  You can uncomment this when you only want to get the location once, not repeatedly
-        manager.stopUpdatingLocation()
+        guard let location = locations.last else { return }  //  Use the last location as the location
+        self.location = location
+        self.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 5000, longitudinalMeters: 5000)
     }
     
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        authorizationStatus = manager.authorizationStatus
-        
-        switch manager.authorizationStatus {
-        case .authorizedWhenInUse, .authorizedAlways:
-            print("LocationManager authorization granted.")
-            manager.startUpdatingLocation()
-        case .denied, .restricted:
-            print("LocationManager authorization denied.")
-            errorMessage = "😡📍 LocationManager accessed denied."
-            manager.stopUpdatingLocation()
-        case .notDetermined:
-            print("LocationManager authorization not determined.")
-            manager.requestWhenInUseAuthorization()
-        @unknown default :
-            manager.requestWhenInUseAuthorization()
-        }
-    }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
-        errorMessage = error.localizedDescription
-        print("😡🗺️ ERROR LocationManager: \(errorMessage ?? "n/a")")
-    }
+   
 }
 
 
