9478768f9d7fc857b579446805f5b1bb1c6e633b
JimWallyj <jwalejko@me.com>
2026-07-25 20:56:23 -0700

    Save 14

--- SnacktacularUI/Views/LoginView.swift
+++ SnacktacularUI/Views/LoginView.swift
@@ -28,28 +28,30 @@ struct LoginView: View {
             Image("logo")
                 .resizable()
                 .scaledToFit()
+                .padding()
             
             Group {
-                TextField("email", text: $email)
+                TextField("E-mail", text: $email)
                     .keyboardType(.emailAddress)
                     .autocorrectionDisabled()
                     .textInputAutocapitalization(.never)
                     .submitLabel(.next)
-                    .focused($focusedField, equals: .email)
+                    .focused($focusedField, equals: .email)  //this field is bound to the .email case
                     .onSubmit {
                         focusedField = .password
                     }
-                    .onChange(of: email){
+                    .onChange(of: email){ _ in
                         enableButtons()
                     }
                 
                 SecureField("password", text: $password)
+                    .textInputAutocapitalization(.never)
                     .submitLabel(.done)
-                    .focused($focusedField, equals: .password)
+                    .focused($focusedField, equals: .password)  //  this field is bound to the .password case
                     .onSubmit {
                         focusedField = nil  //  nil will dismiss the keyboard
                     }
-                    .onChange(of: password){
+                    .onChange(of: password){ _ in
                         enableButtons()
                     }
             }
@@ -58,16 +60,22 @@ struct LoginView: View {
                 RoundedRectangle(cornerRadius: 5)
                     .stroke(.gray.opacity(0.5), lineWidth: 2)
             }
+            .padding(.horizontal)
             
             HStack{
-                Button("Sign Up"){
+                Button{
                     register()
+                } label: {
+                    Text("Sign Up")
                 }
                 .padding(.trailing)
                 
-                Button("Log In"){
+                Button{
                     login()
+                }label:{
+                    Text("Log In")
                 }
+                
                 .padding(.leading)
             }
             .buttonStyle(.borderedProminent)
@@ -76,11 +84,12 @@ struct LoginView: View {
             .padding(.top)
             .disabled(buttonDisabled)
         }
-        .padding()
+        
         .alert(alertMessage, isPresented: $showingAlert) {
             Button("OK", role: .cancel){ }
         }
         .onAppear(){
+            //  if logged in when app runs, navigate to the new screen & skip login screen
             if Auth.auth().currentUser != nil{  //  If we're logged in ...
                 print("🪵 Log in successful!")
                 presentSheet = true
@@ -99,7 +108,7 @@ struct LoginView: View {
     
     func register() {
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error{
+            if let error = error{  //login error occurred
                 print("😡 SIGNUP ERROR: \(error.localizedDescription)")
                 alertMessage = "😡 LOGIN ERROR: \(error.localizedDescription)"
                 showingAlert = true
@@ -112,7 +121,7 @@ struct LoginView: View {
     
     func login(){
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
-            if let error = error{
+            if let error = error{  //  login error occurred
                 print("😡 LOGIN ERROR: \(error.localizedDescription)")
                 alertMessage = "😡 LOGIN ERROR: \(error.localizedDescription)"
                 showingAlert = true
@@ -124,6 +133,8 @@ struct LoginView: View {
     }
 }
 
-#Preview {
-    LoginView()
+struct LoginView_Previews: PreviewProvider{
+    static var previews: some View{
+        LoginView()
+    }
 }
