f3c5a8f97c5992adc4ae849ef0206fa3a84cc26a
JimWallyj <jwalejko@me.com>
2026-07-26 20:55:37 -0700

    Save #22

--- SnacktacularUI/Views/StarsSelectionView.swift
+++ SnacktacularUI/Views/StarsSelectionView.swift
@@ -8,36 +8,41 @@
 import SwiftUI
 
 struct StarsSelectionView: View {
-    @State var rating: Int  //  chnage this to @Binding after layout is tested
+    @Binding var rating: Int // change this to @Binding after layout is tested
+    @State var interactive = true
     let highestRating = 5
     let unselected = Image(systemName: "star")
     let selected = Image(systemName: "star.fill")
-    let font: Font = .largeTitle
+    var font: Font = .largeTitle
     let fillColor: Color = .red
     let emptyColor: Color = .gray
     
     var body: some View {
-        HStack{
+        HStack {
             ForEach(1...highestRating, id: \.self) { number in
                 showStar(for: number)
                     .foregroundColor(number <= rating ? fillColor : emptyColor)
                     .onTapGesture {
-                        rating = number
+                        if interactive {
+                            rating = number
+                        }
                     }
             }
             .font(font)
         }
     }
     
-    func showStar( for number: Int) -> Image{
-        if number > rating{
+    func showStar( for number: Int) -> Image {
+        if number > rating {
             return unselected
-        }else{
+        } else {
             return selected
         }
     }
 }
 
-#Preview {
-    StarsSelectionView(rating: 4)
+struct StarsSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarsSelectionView(rating: .constant(4))
+    }
 }
