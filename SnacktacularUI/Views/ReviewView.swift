461d54e425b0f78cffde6274a73765a35adc287c
JimWallyj <jwalejko@me.com>
2026-07-26 20:53:35 -0700

    Save #21

--- SnacktacularUI/Views/ReviewView.swift
+++ SnacktacularUI/Views/ReviewView.swift
@@ -6,20 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ReviewView: View {
+    @StateObject var reviewVM = ReviewViewModel()
     @State var spot: Spot
     @State var review: Review
+    @State var postedByThisUser = false
+    @State var rateOrReviewerString = "Click to Rate" // otherwise wil say poster e-mail & date
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
+            VStack (alignment: .leading) {
                 Text(spot.name)
                     .font(.title)
                     .bold()
                     .multilineTextAlignment(.leading)
-                    .lineLimit(1)
+                .lineLimit(1)
                 
                 Text(spot.address)
                     .padding(.bottom)
@@ -27,27 +31,31 @@ struct ReviewView: View {
             .padding(.horizontal)
             .frame(maxWidth: .infinity, alignment: .leading)
             
-            Text("Click to Rate:")
-                .font(.title2)
-                .bold()
-            HStack{
-                StarsSelectionView(rating: review.rating)
+            Text(rateOrReviewerString)
+                .font(postedByThisUser ? .title2 : .subheadline)
+                .bold(postedByThisUser)
+                .minimumScaleFactor(0.5)
+                .lineLimit(1)
+                .padding(.horizontal)
+            HStack {
+                StarsSelectionView(rating: $review.rating)
+                    .disabled(!postedByThisUser) // disable if not posted by this user
                     .overlay {
                         RoundedRectangle(cornerRadius: 5)
-                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                            .stroke(.gray.opacity(0.5), lineWidth: postedByThisUser ? 2 : 0)
                     }
             }
             .padding(.bottom)
             
-            VStack (alignment: .leading){
+            VStack (alignment: .leading) {
                 Text("Review Title:")
                     .bold()
                 
                 TextField("title", text: $review.title)
-                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal, 6)
                     .overlay {
                         RoundedRectangle(cornerRadius: 5)
-                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                            .stroke(.gray.opacity(0.5), lineWidth: postedByThisUser ? 2 : 0.3)
                     }
                 
                 Text("Review")
@@ -58,25 +66,61 @@ struct ReviewView: View {
                     .frame(maxHeight: .infinity, alignment: .topLeading)
                     .overlay {
                         RoundedRectangle(cornerRadius: 5)
-                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                            .stroke(.gray.opacity(0.5), lineWidth: postedByThisUser ? 2 : 0.3)
                     }
             }
+            .disabled(!postedByThisUser)
             .padding(.horizontal)
             .font(.title2)
             
             Spacer()
             
         }
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Cancel"){
-                    dismiss()
-                }
+        .onAppear {
+            if review.reviewer == Auth.auth().currentUser?.email {
+                postedByThisUser = true
+            } else {
+                let reviewPostedOn = review.postedOn.formatted(date: .numeric, time: .omitted)
+                rateOrReviewerString = "by : \(review.reviewer)  on: \(reviewPostedOn)"
             }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button("Save"){
-                    //  TODO: Add save code here
-                    dismiss()
+        }
+        .navigationBarBackButtonHidden(postedByThisUser)
+        .toolbar {
+            if postedByThisUser {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        Task {
+                            let success = await reviewVM.saveReview(spot:spot, review: review)
+                            if success {
+                                dismiss()
+                            } else {
+                                print("😡 ERROR: saving data in ReviewView")
+                            }
+                        }
+                    }
+                }
+
+                if review.id != nil {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Spacer()
+                        
+                        Button {
+                            Task {
+                                let success = await reviewVM.deleteReview(spot: spot, review: review)
+                                if success {
+                                    dismiss()
+                                }
+                            }
+                        } label: {
+                            Image(systemName: "trash")
+                        }
+
+                    }
                 }
             }
         }
@@ -84,13 +128,9 @@ struct ReviewView: View {
 }
 
 struct ReviewView_Previews: PreviewProvider {
-     static var previews: some View {
-         NavigationStack {
-             ReviewView(spot: Spot(name: "Shake Shack", address: "49 Boyleston St., Chestnut Hill, MA 02467"), review: Review())
-         }
+    static var previews: some View {
+        NavigationStack {
+            ReviewView(spot: Spot(name: "Shake Shack", address: "49 Boyleston St., Chestnut Hill, MA 02467"), review: Review())
+        }
     }
 }
-
-//#Preview {
-//    ReviewView()
-//}
