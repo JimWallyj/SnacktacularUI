1d84415222943baaf5ab371f4f6362a303b376eb
JimWallyj <jwalejko@me.com>
2026-07-28 20:11:19 -0700

    Save 22

--- SnacktacularUI/ViewModels/SpotViewModel.swift
+++ SnacktacularUI/ViewModels/SpotViewModel.swift
@@ -9,30 +9,12 @@ import Foundation
 import FirebaseFirestore
 import UIKit
 import FirebaseStorage
+internal import Combine
+
 @MainActor
 
 
-@State var review: Review
 
-var body: some View {
-    VStack (alignment: .leading) {
-        Text(review.title)
-            .font(.title2)
-        HStack {
-            StarsSelectionView(rating: $review.rating, interactive: false, font: .callout)
-            Text(review.body)
-                .font(.callout)
-                .lineLimit(1)
-        }
-    }
-}
-}
-
-struct SpotReviewRowView_Previews: PreviewProvider {
-static var previews: some View {
-    SpotReviewRowView(review: Review(title: "Fantastic Food!", body: "I love this place so much! The only thing preventing 5 stars is the surley service", rating: 4))
-}
-}
 
 
 
